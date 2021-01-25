e5ce1323200cc6d69dd11f5d1069d722709097e8
zac <codinfreakgf@gmail.com>
2021-01-25 14:43:47 -0500

    removed main, added pointer casting

--- src/main.c
+++ src/main.c
@@ -3,55 +3,6 @@
 #include "utility.c"
 #include <stdio.h>
 
-// int main(){
-//     Model* model = newModel();
-//     ModelInfo* noiseCancel = &supportedModels[NoiseCancel];
-//     TF_Code code = TF_OK;
-//     code=logStatus(loadModel(model, noiseCancel->directoryLocation, noiseCancel->tag), "loading model");
-//     if(code){
-//         return code;
-//     }
-//     code=logStatus(findModelNodes(model, noiseCancel), "finding graph nodes");
-//     if(code){
-//         return code;
-//     }
-//     TF_Tensor** tensor=(TF_Tensor**)malloc(sizeof(TF_Tensor*));
-//     TF_Tensor* outputTensor=malloc(sizeof(TF_Tensor*));
-//    for(unsigned int i=0; i < noiseCancel->numStaticInputData; ++i){
-//        TFInfo check = dataInfoToTensor(&tensor, &noiseCancel->staticInputData[i], model->status, i);
-//        if(check.code){
-//            freeTensor(tensor, noiseCancel->numInputNodes);
-//            free(outputTensor);
-//            return NULL;
-//        }
-//    }
-//     float data[1][3][3][3]= {{{{1.0f,1.0f,1.0f},{30.0f,80.0f,60.0f},{20.0f,10.0f,120.0f}},
-//     {{1.0f,1.0f,1.0f},{30.0f,80.0f,60.0f},{20.0f,10.0f,120.0f}},
-//     {{1.0f,1.0f,1.0f},{30.0f,80.0f,60.0f},{20.0f,10.0f,120.0f}}}};
-//     DataInfo data1;
-//     data1.data = data;
-//     data1.dataSize = 27 * sizeof(float);
-//     data1.dataType = TF_FLOAT;
-//     int64_t dim [] = {1,3,3,3}; 
-//     data1.dimensions = dim;
-//     data1.numberOfDimensions = 4;
-//     code=logStatus(dataInfoToTensor(&tensor, &data1, model->status, 1), "input tensor");
-//     if(code){
-//         return code;
-//     }
-//     code=logStatus(run(model, noiseCancel, tensor, &outputTensor), "running model");
-//     if(code){
-//         return code;
-//     }
-//     float* buffer = (float *)TF_TensorData(outputTensor);
-//     int numDims = TF_NumDims(outputTensor);
-//     printf("%i \n", numDims);
-//     for(unsigned int i=0; i < numDims; ++i){
-//         printf("%i \n", TF_Dim(outputTensor, i));
-//     }
-//     freeModel(model);
-//     return 0;
-// }
 // globals
 Model* model = NULL;
 ModelInfo* modelInfo = NULL;
@@ -108,11 +59,11 @@ EXPORTED OutputData* runModel(const char* errorMsg, DataInfo dataInfo){
         return NULL;
     }
     // create outputdata to hold all the information required
-    OutputData *outputData = malloc(sizeof(outputData));
+    OutputData *outputData = (OutputData *)malloc(sizeof(outputData));
     outputData->buffer = (float *)TF_TensorData(outputTensor);
     outputData->outputTensor = outputTensor;
     outputData->numOfDimensions = TF_NumDims(outputTensor);
-    outputData->dimension = malloc(sizeof(float) * outputData->numOfDimensions);
+    outputData->dimension = (float *)malloc(sizeof(float) * outputData->numOfDimensions);
     for(unsigned int i=0; i < outputData->numOfDimensions; ++i){
         outputData->dimension[i] = TF_Dim(outputTensor, i);
     }
